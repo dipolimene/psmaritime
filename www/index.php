703a18e4dfb89337d29bc2e30e272fe11a272f0d
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 16:54:38 +0100

    included all bootstrap link

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$page_title = "P'S Maritime - Home";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> <?php echo $page_title; ?> </title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">      
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+
+    <!-- =======================================================
+        Theme Name: Gp
+        Theme URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-templat/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->  
+    
+  </head>
+  <body class="homepage"> 
+
+
+
+
+
+  </body>
+  </html>
