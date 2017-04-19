02229ccd3953b6ea9e627a1339617787eca7139b
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 21:19:40 +0100

    included the service section

--- www/services.php
+++ www/services.php
@@ -59,3 +59,62 @@ $page_title = "P'S Maritime - Services";
     </header><!--/header-->
 
 <!-- HEADER ENDS -->
+
+<!-- SERVICE SECTION BEGINS -->
+
+<section id="services" class="service-item">
+
+       <div class="container">
+
+            <div class="center wow fadeInDown">
+                <h2>OUR SERVICE</h2>
+                <p class="lead">Our services include but not limited to the following below</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="media services-wrap wow fadeInDown">
+                        <div class="pull-left">
+                            <img class="img-responsive" src="images/services/services1.png">
+                        </div>
+                        <div class="media-body">
+                            <h3 class="media-heading">Vessel Clearing and Forwarding</h3>
+                            <p>Offering all services involved with vessel clearance for ships that  call   at the  Nigerian  Ports. This includes registering the crew with the immigration authorities and the ship with the local Ports  authorities ;processing of Temporary Importation Permit , pilotage, mooring, clearance inwards and outward   for vessels  on  arrival  issuing  the  statement  of  facts, Notice of readiness and clearance outwards.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="media services-wrap wow fadeInDown">
+                        <div class="pull-left">
+                            <img class="img-responsive" src="images/services/services2.png">
+                        </div>
+                        <div class="media-body">
+                            <h3 class="media-heading">Husbandry and Procurement</h3>
+                            <p>Arranging everything the captain needs onboard from tools, provisions of cash to Master getting the medicine cupboard restocked. We can also manage purchase of all goods and services required for the project at hand.</p>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="media services-wrap wow fadeInDown">
+                        <div class="pull-left">
+                            <img class="img-responsive" src="images/services/services3.png">
+                        </div>
+                        <div class="media-body">
+                            <h3 class="media-heading">Crew Change</h3>
+                            <p>Providing assistance to ship captains with all crew change formalities, assisting crew enroute, arranging transport to and from the airport to the ship and hotel.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div><!--/.row-->
+
+        </div><!--/.container-->
+
+    </section><!--/#services-->
+
+<!-- SERVICE SECTION ENDS -->
