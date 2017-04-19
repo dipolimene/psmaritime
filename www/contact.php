664f435b141a7b0526e2207463e3f711746d5dd3
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 21:29:38 +0100

    included bootstrap navigation and header

---
+++ www/contact.php
@@ -0,0 +1,61 @@
+<?php
+
+$page_title = "P'S Maritime - Services";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> <?php echo $page_title; ?> </title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">      
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!-- =======================================================
+        Theme Name: Gp
+        Theme URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-templat/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->  
+    
+  </head>
+  <body class="homepage">
+
+ <!-- HEADER BEGINS -->
+   
+	<header id="header">
+        <nav class="navbar navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"> <img src="images/pslogo.png"/ height="73px"> </a>
+                </div>
+				
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li class="active"><a href="services.php">Services</a></li>
+                        <li><a href="contact.php">Contact</a></li>                        
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->	
+    </header><!--/header-->
+
+<!-- HEADER ENDS -->
