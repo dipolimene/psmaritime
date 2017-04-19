3eed85dbebfe0243fd3fb258b60158856d34cf12
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 17:27:08 +0100

    psmaritime index page done

--- www/index.php
+++ www/index.php
@@ -9,7 +9,6 @@ $page_title = "P'S Maritime - Home";
 <!DOCTYPE html>
 <html lang="en">
   <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,21 +22,19 @@ $page_title = "P'S Maritime - Home";
     <link href="css/prettyPhoto.css" rel="stylesheet">      
 	<link href="css/style.css" rel="stylesheet">
 	<link href="css/responsive.css" rel="stylesheet">
-
     <!-- =======================================================
         Theme Name: Gp
         Theme URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-templat/
         Author: BootstrapMade
         Author URL: https://bootstrapmade.com
-        Refactored : Dipo Olimene for dipolimene.com
     ======================================================= -->  
     
   </head>
-  <body class="homepage"> 
-
-<!-- THE HEADER SECTION GOES HERE -->
-
-  		<header id="header">
+  <body class="homepage">  
+  
+<!-- HEADER BEGINS -->
+   
+	<header id="header">
         <nav class="navbar navbar-fixed-top" role="banner">
             <div class="container">
                 <div class="navbar-header">
@@ -62,11 +59,11 @@ $page_title = "P'S Maritime - Home";
         </nav><!--/nav-->	
     </header><!--/header-->
 
-<!-- THE HEADER SECTION ENDS HERE -->
+<!-- HEADER ENDS -->
 
 
-<!-- THE SLIDER SECTION GOES HERE -->
-
+<!-- SLIDER BEGINS -->
+	
 	<div class="slider">
 		<div class="container">
 			<div id="about-slider">
@@ -102,19 +99,282 @@ $page_title = "P'S Maritime - Home";
 		</div>
 	</div>
 
-<!-- THE SLIDER SECTION ENDS HERE -->
+<!-- SLIDER BEGINS -->
 
 
+<!-- OTHER FEATURES BEGINS -->
+
+	 <section id="feature" >
+        <div class="container">
+           <div class="center wow fadeInDown">
+                <h2>WELCOME TO PS MARITIME</h2>
+                <p class="lead">PS MARITIME COMPANY NIGERIA LIMITED is an indigenous shipping company incorporated in Nigeria on the 8th of August 2003. The company’s business concept is to provide maritime support functions for International offshore oil and gas production,to own and run assets regarded as necessary or desirable for this and to provide associated services.This we are doing via the acquisition and pilotage of UNGIESHI,a 10,000 tonnes vessel,for wet bulk. The company also render procurement services which include supplies of platform supply vessel, anchor handling, tug supply vessel, rescue and recovery vessel and any other kind of vessel or boat which suites the client’s demand...<a href="#" class="active">Read More</a></p>
+            </div>
+
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="feature-wrap">
+                            <i class="fa fa-laptop"></i>
+                            <h2>OUR VISION</h2>
+                            <h3>To become a leading Company in marine transportation services, supporting the oil and gas industry. Also, to lead in procurement and platform services in Nigeria through sustained customer satisfaction, maintaining value driven services and a positive attitude.</h3>
+                        </div>
+                    </div><!--/.col-md-4-->
+
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="feature-wrap">
+                            <i class="fa fa-comments"></i>
+                            <h2>OUR MISSION</h2>
+                            <h3>To meet challenges and exceed all expectations by transforming opportunities into possibilities in the industry, and also building up capacities of our employees for the growth of our organization.</h3>
+                        </div>
+                    </div><!--/.col-md-4-->
+
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="feature-wrap">
+                            <i class="fa fa-cloud-download"></i>
+                            <h2>OUR OBJECTIVES</h2>
+                            <h3>To achieve profitability, capital growth and safe investment through exceptional operational supply services, prompt and efficient product delivery, as well as cost effectiveness.</h3>
+                        </div>
+                    </div><!--/.col-md-4-->
+                </div><!--/.features-->
+            </div><!--/.row-->    
+        </div><!--/.container-->
+    </section><!--/#feature-->
+
+<!-- OTHER FEATURES ENDS -->
 
 
+<!-- SERVICE SECTION BEGINS / CLIENTS SECTION BEGINS -->
+	
+	 <section id="recent-works">
+        <div class="container">
+            <div class="center wow fadeInDown">
+                <h2>OUR SERVICES</h2>
+                <p class="lead">Vessel Clearing and Forwarding - Offering all services involved with vessel clearance for ships that call at the Nigerian Ports. This includes registering the crew with the immigration authorities and the ship with the local Ports authorities ;processing of Temporary Importation Permit , pilotage, mooring, clearance inwards and outward for vessels on arrival issuing the statement of facts, Notice of readiness and clearance outwards...<a href="#" class="active">Read More</a></p>
+            </div>
+
+<!-- SERVICE SECTION ENDS -->
+            	
+<div class="center wow fadeInDown">
+          <h2>OUR CLIENTS</h2>
+                              
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item1.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Zone 4 Energy</a> </h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item1.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>   
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item2.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Ascon</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item2.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div>
+                        </div>		
+                    </div>
+                </div> 
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item3.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">BTI Logistics</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item3.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>   
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item4.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Japaul Oil</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item4.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>   
+                
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item5.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Masters Energy</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item5.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>   
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item6.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Mobil</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item6.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item7.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">NNPC</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item7.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>   
+
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <div class="recent-work-wrap">
+                        <img class="img-responsive" src="images/portfolio/recent/item8.png" alt="">
+                        <div class="overlay">
+                            <div class="recent-work-inner">
+                                <h3><a href="#">Oando</a></h3>
+                                <p>Our client for several years</p>
+                                <a class="preview" href="images/portfolio/full/item8.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                            </div> 
+                        </div>
+                    </div>
+                </div>         
+            </div><!--/.row-->
+            
+          </div>
+          
+        </div><!--/.container-->
+    </section><!--/#recent-works-->	
+
+<!-- CLIENTS SECTION ENDS -->
 
 
+<!-- BOTTOM SECTION WITH ADDRESS BEGINS -->
+	
+	<section id="bottom">
+        <div class="container wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Company Address</h3>
+                        <ul>
+                            <li>MAIN OFFICE <br/> 3B Oko Awo Street,<br/> Victoria Island, Lagos, <br/> Nigeria.</li>                       </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
 
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Contact Number</h3>
+                        <ul>
+                            <li>(234) 01 460-5542 <br/>(234) 01 460 5546</li>                          
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
 
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Email Address</h3>
+                        <ul>
+                            <li>info@psmaritime.com.ng</li>
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
 
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Quick Link</h3>
+                        <ul>
+                        	<li><a href="#">Home</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Our Services</a></li>
+                            <li><a href="#">Contact Us</a></li>                          
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
+            </div>
+        </div>
+    </section><!--/#bottom-->
 
+<!-- BOTTOM SECTION WITH ADDRESS ENDS -->
 
+<!-- SOCIAL SECTION BEGINS -->
+	
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+			    <div class="col-lg-12">
+				   <div class="social">
+						<ul class="social-share">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
+							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a href="#"><i class="fa fa-skype"></i></a></li>
+						</ul>
+				   </div>
+                </div>
+			</div>
+		</div><!--/.container-->
+	</div><!--/.top-bar-->
 
+<!-- SOCIAL SECTION ENDS -->
 
-  </body>
-  </html>
+<!-- FOOTER SECTION BEGINS -->
+	
+	<footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    2017 &copy P'S Maritime.com.ng All Rights Reserved.
+                    <div class="credits">
+                        
+              <!--          <a href="https://dipolimene.com/">Designed</a> by <a href="https://bootstrapmade.com/">DO</a>	-->
+                        
+                    </div>
+                </div>
+                
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="https://dipolimene.com/">Designed</a> by <a href="https://dipolimene.com/">DO</a></li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+
+<!-- FOOTER SECTION ENDS -->
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>   
+    <script src="js/wow.min.js"></script>
+	<script src="js/main.js"></script>
+    
+</body>
+</html>
