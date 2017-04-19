04a0625e343d3b272bb191e88c1b2bb9f7a2cf7d
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 16:48:43 +0100

    typography styling responsive and script added

---
+++ www/images/include/db.php
@@ -0,0 +1,22 @@
+<?php
+
+define('DBNAME', 'psmaritime');
+define('DBUSER', 'root');
+define('DBPASS', '');
+
+
+try{
+
+	# prepare a pdo instance
+	$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	#set verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+	} catch(PDOException $e) {
+
+		echo $e->getMessage();
+
+	}
+
+?>
