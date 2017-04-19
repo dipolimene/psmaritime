18b9c576a106c6c68ee814379aee1845c82903fa
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 17:37:24 +0100

    included about section

--- www/about.php
+++ www/about.php
@@ -48,8 +48,8 @@ $page_title = "P'S Maritime - About";
 				
                 <div class="collapse navbar-collapse navbar-right">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.php">Home</a></li>
-                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li class="active"><a href="about.php">About Us</a></li>
                         <li><a href="services.php">Services</a></li>
                         <li><a href="contact.php">Contact</a></li>                        
                     </ul>
@@ -59,6 +59,60 @@ $page_title = "P'S Maritime - About";
     </header><!--/header-->
 
 <!-- HEADER ENDS -->
+
+
+ <!-- ABOUT US BEGINS -->
+
+	<section id="about-us">
+        <div class="container">			
+			<div class="skill-wrap clearfix">			
+				<div class="center wow fadeInDown">
+                
+					<h2>ABOUT US<span></span></h2>
+					<p class="lead">P'S MARITIME COMPANY NIGERIA LIMITED is an indigenous shipping company incorporated in Nigeria on the 8th of August 2003.The company’s business concept is to provide maritime support functions for international offshore oil and gas production,to own and run assets regarded as necessary or desirable for this and to provide associated services. This we are doing via the acquisition and pilotage of <strong>UNGIESHI</strong>,a 10,000 tonnes vessel,for wet bulk.</p>
+
+<p class="lead"> The company also render procurement services which include supplies of platform supply vessel, anchor handling, tug supply vessel,rescue and recovery vessel and any other kind of vessel or boat which suites the client’s demand. Added to the above,are the shipping agency services being carried out upon assignment of the ship owners which include</p>
+				</div>
+				
+			<div class="row">
+                
+               <div class="get-started center wow fadeInDown">
+				<h2>OUR VISION</h2>
+				<p class="lead">To become a leading Company in marine transportation services, supporting the oil and gas industry. <br>  Also, to lead in procurement and platform services in Nigeria through sustained customer satisfaction, maintaining value driven services and a positive attitude</p>
+				<div class="request">
+					<h4><a href="#">VISION</a></h4>
+				</div>
+			</div><!--/.get-started-->
+            
+            <div class="get-started center wow fadeInDown">
+				<h2>OUR MISSION</h2>
+				<p class="lead">To meet challenges and exceed all expectations by transforming opportunities into possibilities in the industry, <br>and also building up capacities of our employees for the growth of our organization.</p>
+				<div class="request">
+					<h4><a href="#">MISSION</a></h4>
+				</div>
+			</div><!--/.get-started-->	
+            
+            <div class="get-started center wow fadeInDown">
+				<h2>OUR OBJECTIVES</h2>
+				<p class="lead">To achieve profitability, capital growth and safe investment through exceptional operational supply services, <br>prompt and efficient product delivery, as well as cost effectiveness.</p>
+				<div class="request">
+					<h4><a href="#">OBJECTIVE</a></h4>
+				</div>
+			</div><!--/.get-started-->
+            
+            <div class="get-started center wow fadeInDown">
+				<h2>OUR CORE VALUES</h2>
+				<p class="lead">Our core values are <br> <b>.</b> Resilience <b>.</b> Integrity <b>.</b> Competency .Efficiency</p>
+				<div class="request">
+					<h4><a href="#">VALUE</a></h4>
+				</div>
+			</div><!--/.get-started-->
+			</div>
+		</div>
+	</div>
+
+<!-- ABOUT US ENDS -->
+
 
 
 
