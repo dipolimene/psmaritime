0706e7a7e7a302a563ad15caa263c3c9826480bd
Oladipupo Olimene <dipotope@yahoo.com>
2017-04-19 17:32:10 +0100

    included header section

--- www/about.php
+++ www/about.php
@@ -31,6 +31,35 @@ $page_title = "P'S Maritime - About";
   </head>
   <body class="homepage">
 
+ <!-- HEADER BEGINS -->
+   
+	<header id="header">
+        <nav class="navbar navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"> <img src="images/pslogo.png"/ height="73px"> </a>
+                </div>
+				
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="services.php">Services</a></li>
+                        <li><a href="contact.php">Contact</a></li>                        
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->	
+    </header><!--/header-->
+
+<!-- HEADER ENDS -->
+
 
 
 
